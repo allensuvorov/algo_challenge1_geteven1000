963fe077f7990557d99c9f94e1af61d4a111e974
Allen <allenvs@mail.ru>
2020-02-09 00:49:09 +0300

    added math based solution to math branch

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-var sum = 0;
-for (var i=2; i<=1000; i+=2) {
-    sum += i;
+var x = 1000;
+function sum (x) {
 
-};
-console.log(sum);
+    var sum = 0;
+    for (var i=2; i<=x; i+=2) {
+        sum += i;
+        
+    };
+    console.log(sum);
+}
+
+function math_sum (x) {
+
+    var math_sum = x/2*(x/2+1);
+    console.log(math_sum);
+}
+sum(x);
+math_sum(x);
