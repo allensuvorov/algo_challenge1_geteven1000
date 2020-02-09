28f1953015d81903a38472d8b6091432d5a7a6d0
Allen <allenvs@mail.ru>
2020-02-09 07:09:48 +0300

    added flooring to even number

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var x = 1000;
+var x = 93;
 function sum (x) {
 
     var sum = 0;
@@ -10,7 +10,8 @@ function sum (x) {
 }
 
 function math_sum (x) {
-
+    // floor to even number via substracting the devision by 2 remainder
+    x=x-x%2;
     var math_sum = x/2*(x/2+1);
     console.log(math_sum);
 }
